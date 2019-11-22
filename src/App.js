7de6bee9b363fea7ba5275807995acea981d7ca4
takanori-ikegawa <takanori.ikegawa@pricer.co.jp>
2019-11-23 00:11:56 +0900

    ToDoの登録機能を実装

--- src/App.js
+++ src/App.js
@@ -3,18 +3,79 @@ import './App.css';
 import ToDoListItem from "./ToDoListItem.js"
 
 class App extends Component {
-    render() {
-        return (
-            <div className="APP">
-            <div>
-                <ToDoListItem
-                title="ホームページ作成"
-                description="サークルホームページを今週中に作成する"
-                />
-            </div>
-            </div>
-        );
-    }
+
+  // ToDoListをstateに定義、初期値は []
+  state = {
+    todoList: []
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form
+          className="App-form"
+          onSubmit={e => {
+            // formのデフォルトのイベントをキャンセル
+            e.preventDefault();
+
+            // idがtitleのElementを取得
+            const titleElement = e.target.elements["title"]
+            // idがdescriptionのElementを取得
+            const descriptionElement = e.target.elements["description"];
+
+            // todoList stateに追加
+            this.setState(
+              {
+                todoList: this.state.todoList.concat({
+                  title: titleElement.value,
+                  description: descriptionElement.value
+                })
+              },
+              // stateの変更後に入力した値を空にする
+              () => {
+                titleElement.value = "";
+                descriptionElement.value = "";
+              }
+            )
+          }}
+        >
+          <div>
+            <input
+              id="title"
+              placeholder="title"
+            />
+            <textarea
+              id="description"
+              placeholder="description"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+            >
+              登録
+            </button>
+          </div>
+        </form>
+        <div>
+        {/* todoList配列の要素数分ToDoListItemコンポーネントを展開 */}
+          {this.state.todoList.map(todo => (
+            <ToDoListItem
+              key={todo.title}
+              title={todo.title}
+              description={todo.description}
+              // クリックされたItemをtodoList stateから削除
+              onClick={() => {
+                this.setState({
+                  todoList: this.state.todoList.filter(x => x !== todo)
+                })
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
